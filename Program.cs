65b3e6a0a6e24725091379db6de31559b0ac1631
Joel9715784 <77417721+Joel9715784@users.noreply.github.com>
2021-02-04 12:46:24 -0500

    PRUEBA
    PRUEBA

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Diagnostics;
+namespace hilos_PruebA
+{
+    class program
+    {
+         static void Main(string[] args)
+        {
+            Console.WriteLine("Departamento");
+
+            Console.WriteLine(" ejercicio ");
+            for (var pisos = 12; pisos <13; pisos++) {
+                for (var departamentos = 8; departamentos < 9; departamentos++)
+                {  ///  departamentos 
+                    Random aleatorio = new Random();
+                    int personaas = aleatorio.Next(1,7);
+                }
+            }
+        static void Create(Object data)
+        {
+            var tw = Stopwatch.StartNew();
+         
+            // creamos valor ramdomico 
+            Random aleatorio = new Random();
+            int num = aleatorio.Next(1,13);
+
+            static void Main(string[] args)
+        {
+            Console.WriteLine("Joel valencia ");
+            var suma = 0;
+            for (var pisos = 12; pisos < 13; pisos++)
+            {
+                Console.WriteLine("me encuentro en el piso " + Thread.CurrentThread.ManagedThreadId);
+                for (var departamentos = 8; departamentos < 9; departamentos++)
+                {  ///  departamentos 
+                    Console.WriteLine("me en el departamento  = " + Thread.CurrentThread.ManagedThreadId);
+                    Random aleatorio = new Random();
+                    int personaas = aleatorio.Next(1, 7);
+
+                    suma = suma + personaas;
+
+                }
+            }
+            Console.WriteLine("en el edificio existen alrededor de  = " + suma);
+            Console.Read();
+        }
+            
+            
+          
+            
